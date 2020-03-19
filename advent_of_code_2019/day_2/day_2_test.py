f43bb2595874822a9a4110978eba9fbde3da52c3
jacob-williams-arc <jacob.williams.arc@gmail.com>
2020-03-19 14:17:04 -0700

    starting the unit tests up

--- advent_of_code_2019/day_2/day_2_test.py
+++ advent_of_code_2019/day_2/day_2_test.py
@@ -1,1 +1,7 @@
 import pytest
+from gravity_assist import execute_intcode
+
+def test_program_execution():
+    assert True
+    #assert execute_intcode([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
+    #assert execute_intcode([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
---
+++ advent_of_code_2019/day_2/gravity_assist.py
@@ -0,0 +1,4 @@
+
+
+def executeIntcode(program):
+    return 6
