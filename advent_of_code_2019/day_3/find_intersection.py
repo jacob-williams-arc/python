51d6584c27aa12907c670b643ba695eac63cf0a6
jacob-williams-arc <jacob.williams.arc@gmail.com>
2020-03-20 14:24:06 -0700

    working part 1 solution. horrendously slow, will probably refactor before continuing

---
+++ advent_of_code_2019/day_3/find_intersection.py
@@ -0,0 +1,48 @@
+increment = {'R': (1, 0),
+             'L': (-1, 0),
+             'U': (0, 1),
+             'D': (0, -1)}
+
+
+def followPath(currentPosition, direction):
+    return [sum(x) for x in zip(currentPosition, increment[direction])]
+
+
+def buildWirePath(startingPosition, moves):
+    path = []
+    currentPosition = startingPosition
+    for move in moves:
+        direction, distance = move[0], int(move[1:])
+        while distance > 0:
+            currentPosition = followPath(currentPosition, direction)
+            path.append(currentPosition)
+            distance -= 1
+
+    return path
+
+
+def findIntersections(pathA, pathB):
+    return [x for x in pathA if x in pathB]
+
+
+def findManhattanDistances(intersectionPoints):
+    return [sum([abs(x), abs(y)]) for [x, y] in intersectionPoints]
+
+
+def findMinimumDistanceForWireIntersection(centralPort, wirePathA, wirePathB):
+    return min(findManhattanDistances(findIntersections(
+        buildWirePath(centralPort, wirePathA),
+        buildWirePath(centralPort, wirePathB))))
+
+
+def loadAndProcess(filename):
+    centralPort = (0, 0)
+    with open(filename) as inputFile:
+        paths = inputFile.readlines()
+
+    return findMinimumDistanceForWireIntersection(
+        centralPort,
+        paths[0].split(','),
+        paths[1].split(','))
+
+print(loadAndProcess('./wire_paths'))
