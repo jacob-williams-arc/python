c535c872c2a372888839ed9119c0b0353abdd4b0
jacob-williams-arc <jacob.williams.arc@gmail.com>
2020-03-20 10:07:04 -0700

    solution for day 2 part 2

--- advent_of_code_2019/day_2/gravity_assist.py
+++ advent_of_code_2019/day_2/gravity_assist.py
@@ -1,12 +1,12 @@
 
 def executeIntcode(program):
-    readPosition = 0
-    while program[readPosition] != 99:
-        if program[readPosition] == 1:
-            program[program[readPosition + 3]] = program[program[readPosition + 1]] + program[program[readPosition + 2]]
-        elif program[readPosition] == 2:
-            program[program[readPosition + 3]] = program[program[readPosition + 1]] * program[program[readPosition + 2]]
-        readPosition += 4
+    instructionPointer = 0
+    while program[instructionPointer] != 99:
+        if program[instructionPointer] == 1:
+            program[program[instructionPointer + 3]] = program[program[instructionPointer + 1]] + program[program[instructionPointer + 2]]
+        elif program[instructionPointer] == 2:
+            program[program[instructionPointer + 3]] = program[program[instructionPointer + 1]] * program[program[instructionPointer + 2]]
+        instructionPointer += 4
     return program
 
 def loadProgramFromFile(filename):
@@ -14,5 +14,23 @@ def loadProgramFromFile(filename):
         rawProgram = inputFile.read().split(',')
     return [int(i) for i in rawProgram]
 
-programFinalState = executeIntcode(loadProgramFromFile('./gravity_assist_program_updated'))
+programInitialState = loadProgramFromFile('./gravity_assist_program_updated')
+programFinalState = executeIntcode(programInitialState)
 print("Value at position 0: " + str(programFinalState[0]))
+
+desiredOutput = 19690720
+# noun position 1 in program, between 0 and 99, inclusive
+# verb position 2 in program, between 0 and 99, inclusive
+
+for noun in range(99):
+    for verb in range(99):
+        programInitialState = loadProgramFromFile('./gravity_assist_program_updated')
+        programInitialState[1] = noun
+        programInitialState[2] = verb
+        programFinalState = executeIntcode(programInitialState)
+
+        if(programFinalState[0] == desiredOutput):
+            print("Noun: " + str(noun))
+            print("Verb: " + str(verb))
+            print("Try: " + str((100 * noun) + verb))
+            break;
