4c8f8e5aca658d5966da664ac45ae0bdcb9418fc
jacob-williams-arc <jacob.williams.arc@gmail.com>
2020-03-23 13:16:04 -0700

    added solution to day 3 part 2, with the associated tests. added a gitignore too

--- advent_of_code_2019/day_3/day_3_test.py
+++ advent_of_code_2019/day_3/day_3_test.py
@@ -1,21 +1,36 @@
 import pytest
-from find_intersection import findMinimumDistanceForWireIntersection
+from find_intersection import findMinimumDistanceForWireIntersection, findMinimumDistanceToIntersectionPoints, buildWirePath
+
+centralPort = (0, 0)
 
 # Using test values provided by question
 def test_case_a():
-    centralPort = (0, 0)
     assert findMinimumDistanceForWireIntersection(centralPort,
         ['R8', 'U5', 'L5', 'D3'],
         ['U7', 'R6', 'D4', 'L4']) == 6
 
 def test_case_b():
-    centralPort = (0, 0)
     assert findMinimumDistanceForWireIntersection(centralPort,
         ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
         ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']) == 159
 
 def test_case_c():
-    centralPort = (0, 0)
     assert findMinimumDistanceForWireIntersection(centralPort,
         ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51'],
         ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']) == 135
+
+def test_case_d():
+    pathA = buildWirePath(centralPort,
+        ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'])
+    pathB = buildWirePath(centralPort,
+        ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'])
+
+    assert findMinimumDistanceToIntersectionPoints(pathA, pathB) == 610
+
+def test_case_e():
+    pathA = buildWirePath(centralPort,
+        ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51'])
+    pathB = buildWirePath(centralPort,
+        ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7'])
+
+    assert findMinimumDistanceToIntersectionPoints(pathA, pathB) == 410
--- advent_of_code_2019/day_3/find_intersection.py
+++ advent_of_code_2019/day_3/find_intersection.py
@@ -9,20 +9,20 @@ def followPath(currentPosition, direction):
 
 
 def buildWirePath(startingPosition, moves):
-    path = set()
+    path = []
     currentPosition = startingPosition
     for move in moves:
         direction, distance = move[0], int(move[1:])
         while distance > 0:
             currentPosition = followPath(currentPosition, direction)
-            path.add(tuple(currentPosition))
+            path.append(tuple(currentPosition))
             distance -= 1
 
     return path
 
 
 def findIntersections(pathA, pathB):
-    return pathA.intersection(pathB)
+    return set(pathA) & set(pathB)
 
 
 def findManhattanDistances(intersectionPoints):
@@ -35,14 +35,36 @@ def findMinimumDistanceForWireIntersection(centralPort, wirePathA, wirePathB):
         buildWirePath(centralPort, wirePathB))))
 
 
-def loadAndProcess(filename):
-    centralPort = (0, 0)
+def findMinimumDistanceToIntersectionPoints(pathA, pathB):
+    intersectionPoints = findIntersections(pathA, pathB)
+
+    return min([(pathA.index(x) + 1) + (pathB.index(x) + 1) for x in intersectionPoints])
+
+
+def loadData(filename):
     with open(filename) as inputFile:
         paths = inputFile.readlines()
+
+    return paths
+
+
+def loadAndProcessPart1(centralPort, filename):
+    paths = loadData(filename)
 
     return findMinimumDistanceForWireIntersection(
         centralPort,
         paths[0].split(','),
         paths[1].split(','))
 
-print(loadAndProcess('./wire_paths'))
+
+def loadAndProcessPart2(centralPort, filename):
+    paths = loadData(filename)
+    pathA = buildWirePath(centralPort, paths[0].split(','))
+    pathB = buildWirePath(centralPort, paths[1].split(','))
+
+    return findMinimumDistanceToIntersectionPoints(pathA, pathB)
+
+
+centralPort = (0, 0)
+print(loadAndProcessPart1(centralPort, './wire_paths'))
+print(loadAndProcessPart2(centralPort, './wire_paths'))
