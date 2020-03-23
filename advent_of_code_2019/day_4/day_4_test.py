5349c9dc7cdad6af4c387785bec7986fdfbaad54
jacob-williams-arc <jacob.williams.arc@gmail.com>
2020-03-23 14:53:13 -0700

    day 4 part 2

--- advent_of_code_2019/day_4/day_4_test.py
+++ advent_of_code_2019/day_4/day_4_test.py
@@ -1,16 +1,31 @@
 import pytest
-from decode_password import isValidPassword
+from decode_password import isValidPasswordPart1, isValidPasswordPart2
 
 # Test Values provided in question
 def test_case_a():
-    assert isValidPassword('111111') == True
+    assert isValidPasswordPart1('111111') == True
 
 
 # Test Values provided in question
 def test_case_b():
-    assert isValidPassword('223450') == False
+    assert isValidPasswordPart1('223450') == False
 
 
 # Test Values provided in question
 def test_case_c():
-    assert isValidPassword('123789') == False
+    assert isValidPasswordPart1('123789') == False
+
+
+# Test Values provided in question
+def test_case_a():
+    assert isValidPasswordPart2('112233') == True
+
+
+# Test Values provided in question
+def test_case_b():
+    assert isValidPasswordPart2('123444') == False
+
+
+# Test Values provided in question
+def test_case_c():
+    assert isValidPasswordPart2('111122') == True
--- advent_of_code_2019/day_4/decode_password.py
+++ advent_of_code_2019/day_4/decode_password.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+
 def validLengthRequirement(candidate):
     return len(candidate) == 6
 
@@ -7,6 +9,15 @@ def validAdjacentRequirement(candidate):
         if(candidate[i] == candidate[i + 1]):
             return True
 
+    return False
+
+
+def validAdjacentRequirementModified(candidate):
+    digitCounts = [''.join(value) for key, value in groupby(candidate)]
+    for digit in range(len(digitCounts)):
+        if len(digitCounts[digit]) == 2:
+            return True
+        
     return False
 
 
@@ -20,14 +31,20 @@ def validNumericPatternRequirement(candidate):
     return True
 
 
-def isValidPassword(candidate):
+def isValidPasswordPart1(candidate):
     return (validLengthRequirement(candidate)
         and validAdjacentRequirement(candidate)
         and validNumericPatternRequirement(candidate))
 
 
+def isValidPasswordPart2(candidate):
+    return (validLengthRequirement(candidate)
+        and validAdjacentRequirementModified(candidate)
+        and validNumericPatternRequirement(candidate))
+
+
 validity = []
 for candidate in range(264360, 746325):
-    validity.append(isValidPassword(str(candidate)))
+    validity.append(isValidPasswordPart2(str(candidate)))
 
 print(validity.count(True))
