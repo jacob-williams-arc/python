6fbf53e36e30f77ac1593c24ef1315f04b8e6462
jacob-williams-arc <jacob.williams.arc@gmail.com>
2020-03-23 14:13:57 -0700

    solution to day 4 part 1

--- advent_of_code_2019/day_4/day_4_test.py
+++ advent_of_code_2019/day_4/day_4_test.py
@@ -3,14 +3,14 @@ from decode_password import isValidPassword
 
 # Test Values provided in question
 def test_case_a():
-    assert isValidPassword(111111) == True
+    assert isValidPassword('111111') == True
 
 
 # Test Values provided in question
 def test_case_b():
-    assert isValidPassword(223450) == False
+    assert isValidPassword('223450') == False
 
 
 # Test Values provided in question
 def test_case_c():
-    assert isValidPassword(123789) == False
+    assert isValidPassword('123789') == False
--- advent_of_code_2019/day_4/decode_password.py
+++ advent_of_code_2019/day_4/decode_password.py
@@ -1,6 +1,33 @@
+def validLengthRequirement(candidate):
+    return len(candidate) == 6
+
+
+def validAdjacentRequirement(candidate):
+    for i in range(len(candidate) - 1):
+        if(candidate[i] == candidate[i + 1]):
+            return True
+
+    return False
+
+
+def validNumericPatternRequirement(candidate):
+    previousDigit = 0
+    for digit in map(int, candidate):
+        if digit < previousDigit:
+            return False
+        previousDigit = digit
+
+    return True
 
 
 def isValidPassword(candidate):
-    return True
+    return (validLengthRequirement(candidate)
+        and validAdjacentRequirement(candidate)
+        and validNumericPatternRequirement(candidate))
 
-#input = tuple(264360, 746325)
+
+validity = []
+for candidate in range(264360, 746325):
+    validity.append(isValidPassword(str(candidate)))
+
+print(validity.count(True))
